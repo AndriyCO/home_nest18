542af545b14e1f4a3e24310c79ffdc5637244499
AndriyCO <Aolijnyk73@gmail.com>
2017-07-25 15:55:40 +0300

    a lot of changes in Main

--- ProjektCreatedInEclipse/src/pcie_packaje/Main.java
+++ ProjektCreatedInEclipse/src/pcie_packaje/Main.java
@@ -3,7 +3,7 @@ package pcie_packaje;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("fix a method for main branch");
+		System.out.println("this mani fix bags in main-method for master-branch");
 		
 
 	}
