9c456cea8ebd04d291e9f80d886e0f7361695cb1
AndriyCO <Aolijnyk73@gmail.com>
2017-07-25 15:19:55 +0300

    changes in main branch

--- ProjektCreatedInEclipse/src/pcie_packaje/Main.java
+++ ProjektCreatedInEclipse/src/pcie_packaje/Main.java
@@ -3,7 +3,7 @@ package pcie_packaje;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("this a accessory branch");
 
 	}
 
