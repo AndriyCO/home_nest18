330b38da808acf6ac6e71381d417d8745da66787
AndriyCO <Aolijnyk73@gmail.com>
2017-07-25 15:27:58 +0300

    first cange in method on master-branch

--- ProjektCreatedInEclipse/src/pcie_packaje/Main.java
+++ ProjektCreatedInEclipse/src/pcie_packaje/Main.java
@@ -3,7 +3,8 @@ package pcie_packaje;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("fix a method for main branch");
+		
 
 	}
 
